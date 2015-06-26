19315f9fc3fa22ddec9295c8c129faabfe7d8ce1
EdgeX <tonmoysaikia@live.com>
2015-06-26 14:33:11 +0200

    Adds method for locating marker

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 #include <fstream>
 
 void extendRectangle(cv::Mat image, cv::Rect origRect, cv::Rect subRect);
+cv::Point locateMarker(cv::Mat clrImg);
 
-int main1(int argc, const char** argv)
+
+int main(int argc, const char** argv)
 {
   config::load_config_file();
-  std::cout << config::HAARCASCADE_FACE;
   cv::CascadeClassifier c;
   //c.load("/home/tonmoy/opencv/opencv-2.4.10/data/haarcascades/haarcascade_frontalface_alt2.xml");
   c.load(config::HAARCASCADE_FACE);
@@ -19,11 +20,18 @@ int main1(int argc, const char** argv)
 
   std::vector<cv::Rect> faces;
   c.detectMultiScale(grayImage, faces, 1.1 ,1,CV_HAAR_SCALE_IMAGE, cv::Size(100,100));
-  std::cout<< faces.size();
+  cv::Rect faceRect = faces[0];
+  //draw rectangle around face
+  //rectangle(image, faceRect, cv::Scalar(0,255,0),1,8,0);
+  cv::Mat faceROI = image(faceRect);
+  cv::Point markerPos = locateMarker(faceROI);
+
+  std::cout<< markerPos;
+  std::cout<<faceRect;
   return 0;
 }
 
-int main(int argc, const char** argv)
+int main1(int argc, const char** argv)
 {
   if( argc != 2)
   {
@@ -36,7 +44,6 @@ int main(int argc, const char** argv)
   image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
   cvtColor(image, grayImage, CV_BGR2GRAY);
   equalizeHist(grayImage,grayImage);
-
   std::vector<cv::Rect> faces = f.detect(grayImage, cv::Size(100,100));
   std::cout<< faces.size();
   if(faces.size()==1){
@@ -45,6 +52,7 @@ int main(int argc, const char** argv)
     rectangle(image, faceRect, cv::Scalar(0,255,0),1,8,0);
 
     cv::Mat faceROI = image(faceRect);
+    cv::Point markerPos = locateMarker(faceROI);
     cvtColor(faceROI, faceROI, CV_BGR2GRAY);
 
     imshow("test", faceROI);
@@ -76,4 +84,23 @@ void extendRectangle(cv::Mat image, cv::Rect origRect, cv::Rect subRect)
   cv::Point pt1(origRect.x + subRect.x, origRect.y + subRect.y);
   cv::Point pt2(pt1.x + subRect.width, pt1.y + subRect.height);
   rectangle(image, pt1, pt2, cv::Scalar(255,0,0),1,8,0);
+}
+
+cv::Point locateMarker(cv::Mat clrImg)
+{
+  cv::Mat imgHSV, bgIsolation;
+  cv::Point mCoord;
+  cvtColor(clrImg, imgHSV, cv::COLOR_BGR2HSV);
+  int rh = 255, rl = 100, gh = 255, gl = 0, bh = 70, bl = 0;
+  cv::inRange(imgHSV, cv::Scalar(bl, gl, rl), cv::Scalar(bh, gh, rh), bgIsolation);
+  cv::Moments oMoments = cv::moments(bgIsolation);
+  double dM01 = oMoments.m01;
+  double dM10 = oMoments.m10;
+  double dArea = oMoments.m00;
+  if(dArea > 0)
+  {
+    mCoord.x = dM10 / dArea;
+    mCoord.y = dM01 / dArea;
+  }
+  return mCoord;
 }
